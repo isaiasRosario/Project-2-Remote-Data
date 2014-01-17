7fe5a3a7f73ed3f67683fdcc0222cc68df05f845
r16902 <irosario@fullsail.edu>
2014-01-16 21:31:58 -0500

    removed vars from file

--- Resources/remote_data.js
+++ Resources/remote_data.js
@@ -71,25 +71,3 @@ xhr.open("GET", url);
 xhr.send();
 
 exports.data = table;
-
-// Table Data
-var table = require('remote_data');
-
-// Main Window
-var win = Ti.UI.createWindow({backgroundColor:"#fff"});
-
-// Title View
-var titleView = Ti.UI.createView({
-	backgroundColor:"#FFCC66",
-	top: 20,
-	height: 20
-});
-
-// Title Label
-var titleLabel = Ti.UI.createLabel({
-	text: "Reddit API Data"
-});
-
-titleView.add(titleLabel);
-win.add(table.data, titleView);
-win.open();
